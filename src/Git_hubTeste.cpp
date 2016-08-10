c4c7aaa0ef8470cb54aae6b7aee2afdb67cb56b2
marcio <marcio@ptnkdev04>
2016-08-10 18:50:20 -0300

    Primeiro Commit

---
+++ src/Git_hubTeste.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Git_hubTeste.cpp
+// Author      : marcio
+// Version     :
+// Copyright   : Test Lib VLC
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
